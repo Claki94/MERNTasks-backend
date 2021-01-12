5cd28a4103e61442bb2b57f00d28d4275da62128
Marcel <marcelclavero94@gmail.com>
2021-01-12 19:57:39 +0100

    Connection to MongoDB cluster

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const moongose = require('mongoose');
+require('dotenv').config({ path: 'variables.env' });
+
+const conectarDB = async () => {
+    try {
+        await moongose.connect(process.env.DB_MONGO, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        });
+        console.log('DB Conectada');
+    } catch (error) {
+        console.log(error);
+        process.exit(1); // Detener la app
+    }
+}
+
+module.exports = conectarDB;
